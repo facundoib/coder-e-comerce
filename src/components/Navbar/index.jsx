069de0da86aa3615d23955d53a3e98d2fd29cec8
Facundo Ibarzabal <facundoibarzabal@gmail.com>
2021-03-09 21:34:20 -0300

    Se agrega navegador

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Carrito</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
