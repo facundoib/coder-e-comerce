46487c5201eeb6ef48048336cb6fe34857670503
Facundo Ibarzabal <facundo.ibarzabal@naranjax.com>
2021-03-23 21:51:44 -0300

    Promises and map

---
+++ src/components/Item/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import ItemCount from '../ItemCount'
+
+const Item = (props) => {
+
+    const [ item ] = useState(props.item);
+    
+    return (
+        <div className="col-4 cardItem p-1">
+            <div className="bg-light">
+                <p className="text-center">{item.name}</p>
+                <ItemCount item={item} />
+            </div>
+        </div>
+    )
+}
+
+export default Item
